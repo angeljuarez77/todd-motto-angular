bbfccee0f3b796bc0d6212c50e7a1eaf07e1d118
Angel Juarez <angeljuarez729@gmail.com>
2020-05-02 22:31:39 -0400

    got rid of

--- js/MainController.js
+++ js/MainController.js
@@ -1,12 +1,11 @@
-function MainController($scope) {
-  $scope.name = "Angel";
+function MainController() {
+  this.name = "Angel";
 }
 
 /*
  * This below seems like another way to inject dependencies
  * ($scope is a dependency)
- * the $scope param on the first line seems to be replaced with any arbitrary
- * name when you use this method below though.
+ * the $scope param on the first line seems to be replaced with any arbitrary name though.
 */
 // MainController.$inject = ['$scope'];
 
